f83c6ba476dff07c898bd7716554324476aa587e
Frank <89235551+summerfences@users.noreply.github.com>
2023-07-07 03:53:26 -0500

    Added files

---
+++ Main.java
@@ -0,0 +1,16 @@
+/*
+Java program written with a Swing GUI that
+allows the user to calculate the cost of an iRacing cart,
+as the website is very 1.0 and adding items can be difficult.
+Currently, only USD is supported, and no support for tax
+is included.
+
+Written by Frank T Passantino, July 7 2023
+This one is for the oval gang!
+ */
+public class Main {
+    public static void main(String[] args) {
+        CalculatorGUI gui = new CalculatorGUI();
+        gui.build();
+    }
+}
