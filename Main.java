03945d7ced501f23d31202c9c1d6fcef1d7189f7
Frank <89235551+summerfences@users.noreply.github.com>
2023-07-07 04:37:57 -0500

    Added files

---
+++ Main.java
@@ -0,0 +1,12 @@
+/*
+Java program written with a Swing GUI that allows the
+user to calculate the cost of an iRacing cart. Currently,
+only USD is supported, and no support for tax is included.
+Written by Frank T Passantino, July 6th, 2023
+ */
+public class Main {
+    public static void main(String[] args) {
+        CalculatorGUI gui = new CalculatorGUI();
+        gui.build();
+    }
+}
---
+++ PriceCalculator.java
@@ -0,0 +1,38 @@
+public class PriceCalculator {
+    int tracks1495, tracks1195, cars, combinedQty, legacyCars, legacyTracks;
+    double total, totalCopy;
+    public PriceCalculator() {
+        // Junk number to init
+        totalCopy = 0.00951;
+    }
+    // Reset the total and quantity, emptying the cart
+    public void resetTotal () {
+        total = 0;
+        combinedQty = 0;
+    }
+    // Add the amount of cars to the total cost and quantity
+    public void addCars () {
+        combinedQty += cars;
+        total += (cars * 11.95);
+    }
+    // Add the amount of $14.95 tracks to the total cost and quantity
+    public void add1495Tracks () {
+        combinedQty += tracks1495;
+        total += (tracks1495 * 14.95);
+    }
+    // Add the amount of $11.95 tracks to the total cost and quantity
+    public void add1195Tracks () {
+        combinedQty += tracks1195;
+        total += (tracks1195 * 11.95);
+    }
+    // Add the amount of legacy cars to the total cost and quantity
+    public void addLegacyCars() {
+        combinedQty += legacyCars;
+        total += (legacyCars * 2.95);
+    }
+    // Add the amount of legacy tracks to the total cost and quantity
+    public void addLegacyTracks() {
+        combinedQty += legacyTracks;
+        total += (legacyTracks * 4.95);
+    }
+}
