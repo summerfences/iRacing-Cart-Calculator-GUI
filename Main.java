0a39de92bffb6dfa480d4b1731e775e038bb12a1
Frank <89235551+summerfences@users.noreply.github.com>
2023-07-16 17:05:40 -0500

    Update Main.java
    removed name, added github link

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@
 Java program written with a Swing GUI that allows the
 user to calculate the cost of an iRacing cart. Currently,
 only USD is supported, and no support for tax is included.
-Written by Frank T Passantino, July 6th, 2023
+https://github.com/summerfences 7/6/23 v0.1
  */
 public class Main {
     public static void main(String[] args) {
